2776027a25e7a83d366c44b15e062fd59e664655
sumer singh <sumersingh@PS-MacBook.local>
2018-12-05 20:02:20 -0800

    guess evaluate

--- MasterMind/src/ai/project/common/GuessEvaluator.java
+++ MasterMind/src/ai/project/common/GuessEvaluator.java
@@ -37,12 +37,76 @@ public class GuessEvaluator {
 	 */
 	public int[] evaluate(int[] guess) {
 		if(guess.length != secretCode.length) return null;
-		int[] h = new int[secretCode.length];
+		int[] h = new int[2];
 		Arrays.fill(h, EMPTY);		
 		// TODO logic for evaluation.
-		
+		h[0] = getColorCorrectPositionCorrect(guess, secretCode);
+		h[1] = getColorCorrectPositionIncorrect(guess, secretCode);
+		if (h[0] == secretCode.length) {
+			gameOver = true;
+		}
 		return h;
 	}
+	
+	/***
+	 * check for number of correct colors but in incorrect position form the guess
+	 * @param guess
+	 * @param secretCode
+	 * @return
+	 */
+    public static int getColorCorrectPositionIncorrect(int[] guess, int[] secretCode) {
+        int count = 0;
+        int[] guessA = new int[guess.length];
+        int[] secretA = new int[secretCode.length];
+        
+        // create a copy of the two input arrays
+        System.arraycopy(guess, 0, guessA, 0, guess.length);
+        System.arraycopy(secretCode, 0, secretA, 0, secretCode.length);
+        
+        // check each of the items in corresponding positions
+        for (int i = 0; i < guess.length; ++i) {
+        	// if they are the same, i.e. in the correct position
+            if (guessA[i] == secretA[i]) {
+            	// mark as dealt with (since blacks() handles that) by subtracting the current iteration and an arbitrary sufficiently different value from each
+                guessA[i] = 0 - i - 50;
+                secretA[i] = 0 - i - 100;
+            }
+        }
+        
+        // compare each item in oneA to every item in twoA
+        for (int i = 0; i < guess.length; ++i) {
+            for (int j = 0; j < guess.length; ++j) {
+            	// don't want to check items at the same index - we did that earlier - only ones in different positions
+                if (i != j && guessA[i] == secretA[j]) {
+                	// increment the counter of whites
+                    ++count;
+                    // mark each one as dealt with
+                    guessA[i] = 0 - i - 50;
+                    secretA[j] = 0 - j - 100;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+    
+    /***
+     * check for number of correct colors in correct positions from the guess
+     * @param guess
+     * @param secretCode
+     * @return
+     */
+    public static int getColorCorrectPositionCorrect(int[] guess, int[] secretCode) {
+    	int count = 0;
+    	// iterate over the arrays
+        for (int i = 0; i < guess.length; ++i) {
+        	// if they have the same value at the same point in the array increment the val counter
+            if (guess[i] == secretCode[i]) {
+                ++count;
+            }
+        }
+        return count;
+    }
 	
 	/**
 	 * Returns the game status.
